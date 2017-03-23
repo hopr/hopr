f56e14739ce0703fee0a8d43c0d52a1754b25272
hopr <hopr@users.noreply.github.com>
2017-03-23 10:25:38 +0100

    udev permissions testing for hopr user

---
+++ script/todo/test.sh
@@ -0,0 +1,4 @@
+ls -al /dev/uinput
+echo "python uinput_minimal.py"
+python uinput_minimal.py
+
--- script/todo/uinput_minimal.py
+++ script/todo/uinput_minimal.py
@@ -1,18 +1,13 @@
 from evdev import UInput, ecodes
-
 KEY_RELEASE = 0L
 KEY_PRESS = 1L
 
-def run():    
-    ui = UInput()
-    ui.write(ecodes.EV_KEY, ecodes.KEY_A, KEY_PRESS)
-    ui.write(ecodes.EV_KEY, ecodes.KEY_A, KEY_RELEASE)
-    ui.syn()
-    ui.close()
+ui = UInput()
+ui.write(ecodes.EV_KEY, ecodes.KEY_A, KEY_PRESS)
+ui.write(ecodes.EV_KEY, ecodes.KEY_A, KEY_RELEASE)
+ui.syn()
+ui.close()
 
-
-if __name__ == "__main__":
-    run()
 
 
 
